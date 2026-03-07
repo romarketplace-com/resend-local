d3cc52c7d11268c361120cbcb8c3aae93bda8089
Pavel Kudinov <pkudinov@gmail.com>
2026-03-06 22:09:26 -0800

    chore: upgrade next.js to 16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,11 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import eslintPluginUnusedImports from "eslint-plugin-unused-imports";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["dist/**", "requests/**"],
+  },
+  ...nextCoreWebVitals,
   {
     plugins: {
       "unused-imports": eslintPluginUnusedImports,
